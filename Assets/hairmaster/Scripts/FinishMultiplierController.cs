55447d36a240dd75e97c2c16a6caa8287f844dc3
nhulemis <nhulemis@gmail.com>
2022-06-11 11:13:28 +0700

    x

--- Assets/hairmaster/Scripts/FinishMultiplierController.cs
+++ Assets/hairmaster/Scripts/FinishMultiplierController.cs
@@ -51,6 +51,18 @@ namespace hairmaster.Scripts
 
             public override string ToString() => Title;
         }
+        class Bookxhuwxccw
+        {
+            public string Title { get; }
+            public string Publisher { get; }
+            public string? Isbn { get; }
+            
+
+            public void Deconstruct(out string title, out string publisher, out string? isbn)
+                => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+            public override string ToString() => Title;
+        }
         public void setMaterial()
         {
        
