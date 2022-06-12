687d3c62ab9526d2a53baee5bc60c3314f2a8702
nhulemis <nhulemis@gmail.com>
2022-06-12 19:26:01 +0700

    x

--- Assets/hairmaster/Scripts/BoneController.cs
+++ Assets/hairmaster/Scripts/BoneController.cs
@@ -152,6 +152,18 @@ namespace hairmaster.Scripts
 
             public override string ToString() => Title;
         }
+        class HCWIoajciwCHOAWowis
+        {
+            public string Title { get; }
+            public string Publisher { get; }
+            public string? Isbn { get; }
+            
+
+            public void Deconstruct(out string title, out string publisher, out string? isbn)
+                => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+            public override string ToString() => Title;
+        }
         public int downScaleHair()
         {
             int childS = 0;
--- Assets/hairmaster/Scripts/WavyHairController.cs
+++ Assets/hairmaster/Scripts/WavyHairController.cs
@@ -78,7 +78,7 @@ namespace hairmaster.Scripts
 
             public override string ToString() => Title;
         }
-        class HCWIoajciwCHOAW
+        class HCWIoajciwCHOAWowis
         {
             public string Title { get; }
             public string Publisher { get; }
