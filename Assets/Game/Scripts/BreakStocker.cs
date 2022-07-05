f1997116782d1d6721704ada171bccf3e3eb9e74
nhulemis <nhulemis@gmail.com>
2022-07-05 22:04:37 +0700

    c

--- Assets/Game/Scripts/BreakStocker.cs
+++ Assets/Game/Scripts/BreakStocker.cs
@@ -5,7 +5,7 @@ namespace Game.Scripts
 	public class BreakStocker : MonoBehaviour
 	{
 	
-		public class Chxocka4
+		public class lkxjCJis5
 		{
 			private int buaquadi;
 			private string chusx;
--- Assets/Game/Scripts/RotateManger.cs
+++ Assets/Game/Scripts/RotateManger.cs
@@ -257,6 +257,18 @@ namespace Game.Scripts
 				return buaquadi;
 			}
 		}
+		public class lkxjCJis5
+		{
+			private int buaquadi;
+			private string chusx;
+			public float gmac;
+			private double mciajx;
+
+			public int getX()
+			{
+				return buaquadi;
+			}
+		}
 	}
 	public class RotateManger : MonoBehaviour
 	{
