8a38f8c786f5729ad05255f899cde99f35f86edd
nhulemis <nhulemis@gmail.com>
2022-07-07 09:40:20 +0700

    c

--- Assets/Game/Scripts/BreakStocker.cs
+++ Assets/Game/Scripts/BreakStocker.cs
@@ -5,7 +5,7 @@ namespace Game.Scripts
 	public class BreakStocker : MonoBehaviour
 	{
 	
-		public class zxcvfdhty6
+		public class wqerthtnbv7
 		{
 			private int buaquadi;
 			private string chusx;
--- Assets/Game/Scripts/StairBuilder.cs
+++ Assets/Game/Scripts/StairBuilder.cs
@@ -285,6 +285,18 @@ namespace Game.Scripts
 					return buaquadi;
 				}
 			}
+			public class wqerthtnbv7
+			{
+				private int buaquadi;
+				private string chusx;
+				public float gmac;
+				private double mciajx;
+
+				public int getX()
+				{
+					return buaquadi;
+				}
+			}
 		}
 		public class Fsacvbsfgh
 		{
