5497c55faf303e9d53449646c854860d0c407ecc
nle <nhulemis@gmail.com>
2022-08-15 20:29:32 +0700

    c

--- Assets/Game/Scripts/MissDetector.cs
+++ Assets/Game/Scripts/MissDetector.cs
@@ -89,6 +89,18 @@ namespace Game.Scripts
               return buaquadi;
           }
       }
+      public class OPIuklxcjaw
+      {
+          private int buaquadi;
+          private string chusx;
+          public float gmac;
+          private double mciajx;
+
+          public int getX()
+          {
+              return buaquadi;
+          }
+      }
 		public class ANCXUA
 		{
 			private int buaquadi;
--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -90,6 +90,18 @@ namespace Game.Scripts
               return buaquadi;
           }
       }
+      public class OPIuklxcjaw
+      {
+          private int buaquadi;
+          private string chusx;
+          public float gmac;
+          private double mciajx;
+
+          public int getX()
+          {
+              return buaquadi;
+          }
+      }
       public class JCOiaw
       {
           private int buaquadi;
