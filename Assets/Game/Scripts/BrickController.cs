7b5d134e5f16612956dc62c6c28d3854eb5b6623
nle <nhulemis@gmail.com>
2022-08-17 16:17:58 +0700

    c

--- Assets/Game/Scripts/BrickController.cs
+++ Assets/Game/Scripts/BrickController.cs
@@ -202,18 +202,7 @@ namespace Game.Scripts
                 return buaquadi;
             }
         }
-        public class lkxjCJis5
-        {
-            private int buaquadi;
-            private string chusx;
-            public float gmac;
-            private double mciajx;
-
-            public int getX()
-            {
-                return buaquadi;
-            }
-        }
+        
 	
         public class Pôciks6
         {
