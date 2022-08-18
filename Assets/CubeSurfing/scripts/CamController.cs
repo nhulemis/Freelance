01ba0586a618db02e422dda4051350bdd5d3ccb4
nle <nhulemis@gmail.com>
2022-08-18 08:38:57 +0700

    c

--- Assets/CubeSurfing/scripts/CamController.cs
+++ Assets/CubeSurfing/scripts/CamController.cs
@@ -88,6 +88,19 @@ namespace CubeSurfing.scripts
 					return buaquadi;
 				}
 			}
+
+        public class WVZLSJBZOJKHK
+			{
+				private int buaquadi;
+				private string chusx;
+				public float gmac;
+				private double mciajx;
+
+				public int getX()
+				{
+					return buaquadi;
+				}
+			}
    private double D;
             private float E;
 
--- Assets/_Scripts/OnCar.cs
+++ Assets/_Scripts/OnCar.cs
@@ -134,6 +134,19 @@ public class OnCar : MonoBehaviour
 					return buaquadi;
 				}
 			}
+
+        public class WVZLSJBZOJKHK
+			{
+				private int buaquadi;
+				private string chusx;
+				public float gmac;
+				private double mciajx;
+
+				public int getX()
+				{
+					return buaquadi;
+				}
+			}
    private float tempSpeed;
     // Start is called before the first frame update
     void Start()
