c8d7a27edd427215a180af79943a0d9c93227035
nle <nhulemis@gmail.com>
2022-08-30 00:48:00 +0700

    c

--- Assets/HighHeel 1/Scripts/UiManager.cs
+++ Assets/HighHeel 1/Scripts/UiManager.cs
@@ -575,7 +575,7 @@ public class UiManager : MonoBehaviour
     {
         AudioController.instance.buttonClick();
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(currentLevelIndex);
     }
     public void startPlay()
     {
