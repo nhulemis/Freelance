0684a66fbad1cbdf0e30ee4937b2863f890d0b0d
nle <nhulemis@gmail.com>
2022-08-29 22:52:47 +0700

    c

---
+++ Assets/Store/Scripts/Editor/MyPostProcess.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace Game.Scripts.Editor
+{
+    public class MyPostProcess
+    {
+        [PostProcessBuildAttribute(999)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+            Debug.Log( pathToBuiltProject );
+
+            
+
+            string appName = Application.productName.Split('-')[1].Split(" ")[0];
+            
+            // var apks = Directory.GetFiles(pathToBuiltProject , "*.aab | *.apk");
+
+            //foreach (var apk in apks)
+            {
+                string extension = Path.GetExtension(pathToBuiltProject);
+                string newPath = Application.dataPath + "/../ScreenShots/" + Application.productName + extension;
+                
+                File.Copy(pathToBuiltProject,newPath,true);
+            }
+            
+        }
+    }
+}
