e0cc9c1f45bd8681fa4fa26272b7087290834d8c
nhulemis <45124251+nhulemis@users.noreply.github.com>
2022-09-22 18:13:47 +0700

    3

--- Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
+++ Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
@@ -10762,6 +10762,48 @@ namespace LayerLab
         string chars = string.Empty;
         return chars;
     }
+
+        public class CyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW
+    {
+        private int buaquadi;
+        private string chusx;
+        public float gmac;
+        private double mciajx;
+
+        public int getX()
+        {
+            return buaquadi;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+        public string? Isbn { get; }
+
+        public CyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW()
+        {
+            
+        }
+        public CyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW(string title, string publisher, string? isbn)
+            => (Title, Publisher, Isbn) = (title, publisher, isbn);
+
+        public CyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW(string title, string publisher)
+            : this(title, publisher, null) {}
+
+        public void Deconstruct(out string title, out string publisher, out string? isbn)
+            => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+        public override string ToString() => Title;
+    }
+
+    public CyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW GetCyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW(){
+        var clasx = new CyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW();
+        return  clasx;
+    }
+
+    public string RandomStringCyOoXkzWmEZXmYmhcipmIVbJNVIiDcFsDRxgdnEasvAILLdyCzW(int length)
+    {
+        string chars = string.Empty;
+        return chars;
+    }
  private void Start()
         {
             textTitle = transform.GetComponentInChildren<TextMeshProUGUI>();
--- Assets/_ROOF/Scripts/ConFetiController.cs
+++ Assets/_ROOF/Scripts/ConFetiController.cs
@@ -1396,6 +1396,48 @@ public class ConFetiController : MonoBehaviour
         string chars = string.Empty;
         return chars;
     }
+
+        public class PPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC
+    {
+        private int buaquadi;
+        private string chusx;
+        public float gmac;
+        private double mciajx;
+
+        public int getX()
+        {
+            return buaquadi;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+        public string? Isbn { get; }
+
+        public PPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC()
+        {
+            
+        }
+        public PPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC(string title, string publisher, string? isbn)
+            => (Title, Publisher, Isbn) = (title, publisher, isbn);
+
+        public PPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC(string title, string publisher)
+            : this(title, publisher, null) {}
+
+        public void Deconstruct(out string title, out string publisher, out string? isbn)
+            => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+        public override string ToString() => Title;
+    }
+
+    public PPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC GetPPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC(){
+        var clasx = new PPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC();
+        return  clasx;
+    }
+
+    public string RandomStringPPExhXFbBbQTKzhTVbFDoePuNVSxBsJgDGpczTPJYMC(int length)
+    {
+        string chars = string.Empty;
+        return chars;
+    }
    void Start()
     {
         
