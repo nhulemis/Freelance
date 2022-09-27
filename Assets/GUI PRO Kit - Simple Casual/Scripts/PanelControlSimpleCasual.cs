243735ce6dc90f098f131459a6719cbdd740c8c2
nhulemis <45124251+nhulemis@users.noreply.github.com>
2022-09-27 18:52:53 +0700

    8

--- Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
+++ Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
@@ -13114,6 +13114,48 @@ namespace LayerLab
         string chars = string.Empty;
         return chars;
     }
+
+        public class LsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn
+    {
+        private int buaquadi;
+        private string chusx;
+        public float gmac;
+        private double mciajx;
+
+        public int getX()
+        {
+            return buaquadi;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+        public string? Isbn { get; }
+
+        public LsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn()
+        {
+            
+        }
+        public LsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn(string title, string publisher, string? isbn)
+            => (Title, Publisher, Isbn) = (title, publisher, isbn);
+
+        public LsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn(string title, string publisher)
+            : this(title, publisher, null) {}
+
+        public void Deconstruct(out string title, out string publisher, out string? isbn)
+            => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+        public override string ToString() => Title;
+    }
+
+    public LsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn GetLsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn(){
+        var clasx = new LsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn();
+        return  clasx;
+    }
+
+    public string RandomStringLsSpzfQuczNqarFFXMZvpnOrwtrsfbqxrVrcGMTsysNpVYBlOeWOVANXeasFn(int length)
+    {
+        string chars = string.Empty;
+        return chars;
+    }
  private void Start()
         {
             textTitle = transform.GetComponentInChildren<TextMeshProUGUI>();
