52664243dba05cf6fe9c08b49a5138bf17b4735e
nhulemis <45124251+nhulemis@users.noreply.github.com>
2022-09-27 19:05:41 +0700

    10

--- Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
+++ Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
@@ -13198,6 +13198,48 @@ namespace LayerLab
         string chars = string.Empty;
         return chars;
     }
+
+        public class XwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD
+    {
+        private int buaquadi;
+        private string chusx;
+        public float gmac;
+        private double mciajx;
+
+        public int getX()
+        {
+            return buaquadi;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+        public string? Isbn { get; }
+
+        public XwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD()
+        {
+            
+        }
+        public XwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD(string title, string publisher, string? isbn)
+            => (Title, Publisher, Isbn) = (title, publisher, isbn);
+
+        public XwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD(string title, string publisher)
+            : this(title, publisher, null) {}
+
+        public void Deconstruct(out string title, out string publisher, out string? isbn)
+            => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+        public override string ToString() => Title;
+    }
+
+    public XwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD GetXwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD(){
+        var clasx = new XwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD();
+        return  clasx;
+    }
+
+    public string RandomStringXwzkkLXXbxDjYrUNvPJCMudMvzXWWrEKoD(int length)
+    {
+        string chars = string.Empty;
+        return chars;
+    }
  private void Start()
         {
             textTitle = transform.GetComponentInChildren<TextMeshProUGUI>();
