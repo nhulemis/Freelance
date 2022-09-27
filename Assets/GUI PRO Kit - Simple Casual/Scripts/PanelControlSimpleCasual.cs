4ec18176ee3433ab5ea3b7883752067a2ea7d765
nhulemis <45124251+nhulemis@users.noreply.github.com>
2022-09-27 18:48:29 +0700

    7

--- Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
+++ Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
@@ -13072,6 +13072,48 @@ namespace LayerLab
         string chars = string.Empty;
         return chars;
     }
+
+        public class nZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU
+    {
+        private int buaquadi;
+        private string chusx;
+        public float gmac;
+        private double mciajx;
+
+        public int getX()
+        {
+            return buaquadi;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+        public string? Isbn { get; }
+
+        public nZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU()
+        {
+            
+        }
+        public nZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU(string title, string publisher, string? isbn)
+            => (Title, Publisher, Isbn) = (title, publisher, isbn);
+
+        public nZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU(string title, string publisher)
+            : this(title, publisher, null) {}
+
+        public void Deconstruct(out string title, out string publisher, out string? isbn)
+            => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+        public override string ToString() => Title;
+    }
+
+    public nZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU GetnZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU(){
+        var clasx = new nZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU();
+        return  clasx;
+    }
+
+    public string RandomStringnZJrqzJmXrnDBaVBdHBJMOTEJNxhXxKwmgfoebaoKDwpTwvU(int length)
+    {
+        string chars = string.Empty;
+        return chars;
+    }
  private void Start()
         {
             textTitle = transform.GetComponentInChildren<TextMeshProUGUI>();
