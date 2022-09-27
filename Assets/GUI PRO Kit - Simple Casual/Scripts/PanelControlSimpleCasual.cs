a406a3f142e42fa4683b8dc5326fe6d1cb8a0d76
nhulemis <45124251+nhulemis@users.noreply.github.com>
2022-09-27 18:11:57 +0700

    2

--- Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
+++ Assets/GUI PRO Kit - Simple Casual/Scripts/PanelControlSimpleCasual.cs
@@ -12862,6 +12862,48 @@ namespace LayerLab
         string chars = string.Empty;
         return chars;
     }
+
+        public class dIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz
+    {
+        private int buaquadi;
+        private string chusx;
+        public float gmac;
+        private double mciajx;
+
+        public int getX()
+        {
+            return buaquadi;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+        public string? Isbn { get; }
+
+        public dIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz()
+        {
+            
+        }
+        public dIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz(string title, string publisher, string? isbn)
+            => (Title, Publisher, Isbn) = (title, publisher, isbn);
+
+        public dIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz(string title, string publisher)
+            : this(title, publisher, null) {}
+
+        public void Deconstruct(out string title, out string publisher, out string? isbn)
+            => (title, publisher, isbn) = (Title, Publisher, Isbn);
+
+        public override string ToString() => Title;
+    }
+
+    public dIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz GetdIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz(){
+        var clasx = new dIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz();
+        return  clasx;
+    }
+
+    public string RandomStringdIRrWeYxEQaYwFCdqsWJEgJwhecjcwZiGfqdIfHEaNCeIQYdOz(int length)
+    {
+        string chars = string.Empty;
+        return chars;
+    }
  private void Start()
         {
             textTitle = transform.GetComponentInChildren<TextMeshProUGUI>();
